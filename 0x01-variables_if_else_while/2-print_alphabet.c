437b893755b003ccd582469325c49b78b437c95a
abdulrashaad <rashardharuna16@gmail.com>
2023-06-18 15:21:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: return 0 passed
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'b';
+
+	while
+		(letter <= 'x') {
+			putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
