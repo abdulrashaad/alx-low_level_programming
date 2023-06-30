7e9891aebfebc9d994cfd3fd16c6cf2019d63ac9
abdulrashaad <rashardharuna16@gmail.com>
2023-06-29 17:00:51 -0700

    rashard

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - fuction encode to 13337 speak
+ * @n: input values
+ * Return: value of n
+ */
+
+char *leet(char *n)
+{
+	int x, y;
+
+	char s1[] = "aAeEotTIl";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
