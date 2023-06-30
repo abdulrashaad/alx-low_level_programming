fcaaebcc0da5ec633c5f3b24f785a50d1e1c7205
abdulrashaad <rashardharuna16@gmail.com>
2023-06-29 18:08:35 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	int a[5];
+
+	int *p;
+
+	x[2] = 1024;
+
+	z = &x;
+
+	/*
+	 * write your code of line here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * -you are not allowed to modify p
+	 *  -only one statement
+	 *  -you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+
+	/* ...so that this prints 98\n */
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
