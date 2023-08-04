a3c04f158d5018c986fc5d4d86b287ec448a1e90
abdulrashaad <rashardharuna16@gmail.com>
2023-08-03 19:04:13 -0700

    get bit value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returning of the bit value at an index
+ * of decimal number
+ * @n: n represents number to look for
+ * @index: represents the bits inde
+ * Return: bit value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int napo;
+
+	if (index > 63)
+		return (-1);
+
+	napo = (n >> index) & 1;
+
+	return (napo);
+}
